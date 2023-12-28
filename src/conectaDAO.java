d52720af91b9d89b3983058c9c19d26d12ee285c
venecian@gmail.com <venecian@gmail.com>
2023-12-27 22:09:52 -0300

    versao inicial

---
+++ src/conectaDAO.java
@@ -0,0 +1,22 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class conectaDAO {
+    
+    public Connection connectDB(){
+        Connection conn = null;
+        
+        try {
+        
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=mysql");
+            
+        } catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
