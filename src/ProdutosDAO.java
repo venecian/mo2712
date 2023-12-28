662fab27c9d5cd4684c2b3f2cec1b3be54e27ec6
venecian@gmail.com <venecian@gmail.com>
2023-12-27 22:19:49 -0300

    Botão cadastrar produto funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -21,11 +22,29 @@ public class ProdutosDAO {
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    conectaDAO conecta;
+    
+    public ProdutosDAO() {
+        this.conecta = new conectaDAO();
+        this.conn = this.conecta.connectDB();
+    }
+    
     
     public void cadastrarProduto (ProdutosDTO produto){
+                
+        String sql = "INSERT INTO produtos (nome,valor,status)VALUES(?,?,?)";
         
-        
-        //conn = new conectaDAO().connectDB();
+        try {
+            PreparedStatement prep = this.conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso");
+            
+        } catch (SQLException ex) {
+            System.out.println("Não foi possível cadastrar o produto " + ex.getMessage());
+        }
         
         
     }
